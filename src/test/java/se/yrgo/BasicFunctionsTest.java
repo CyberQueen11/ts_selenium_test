fd059c223a961ce193d11e3960e87352a2775c4e
Älva <alvlac0326@skola.goteborg.se>
2024-11-28 17:25:26 +0100

    donePageShowsDoneList is implemented and correctly tested

--- src/test/java/se/yrgo/BasicFunctionsTest.java
+++ src/test/java/se/yrgo/BasicFunctionsTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,10 +17,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasicFunctionsTest {
 
     WebDriver driver;
+    Duration fiveSeconds = Duration.ofSeconds(5);
 
     @BeforeEach
     void setupTest() {
@@ -33,7 +37,7 @@ public class BasicFunctionsTest {
 
     @Test
     void checkTitle() {
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(fiveSeconds);
 
         driver.get("https://yrgo-amazing-todo-app.netlify.app/");
 
@@ -42,16 +46,37 @@ public class BasicFunctionsTest {
 
     @Test
     void homePageShowNotDoneList() {
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(fiveSeconds);
         driver.get("https://yrgo-amazing-todo-app.netlify.app/");
 
         WebElement todoList = driver.findElement(By.className("todolist"));
         assertNotEquals("todolist__done", todoList.getClass().getName());
     }
+
+    @Test
+    void donePageShowsDoneList() {
+        driver.manage().timeouts().implicitlyWait(fiveSeconds);
+        driver.get("https://yrgo-amazing-todo-app.netlify.app/");
+
+        final var wait = new WebDriverWait(driver, fiveSeconds);
+
+        final var edLink = Utils.find(driver, By.linkText("Done"));
+        wait.until(ExpectedConditions.elementToBeClickable(edLink));
+        edLink.click();
+
+        wait.until(ExpectedConditions.urlContains("/done"));
+        assertTrue(driver.getCurrentUrl().contains("/done"));
+
+        List<WebElement> todos = driver.findElements(By.cssSelector(".todolist li"));
+
+        for (WebElement todo : todos) {
+            WebElement checkbox = todo.findElement(By.cssSelector("input[type='checkbox']"));
+            assertTrue(checkbox.isSelected());
+        }
+    }
 }
 
 /**
- * Only todos marked as "not done" should be displayed on the homepage.
  * Only todos marked as "done" should be displayed on the "done" page.
  * You can add a new todo, which will initially be marked as "not done."
  * A todo can be marked as "done" using a checkbox, and it will then move to the
